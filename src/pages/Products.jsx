836babfdec12e7df0533ad70057cd37dda331982
saepulfariz <saepulfariz108@gmail.com>
2024-07-11 15:16:38 +0700

    add show data products api

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,36 @@
 import { Helmet } from "react-helmet";
+import { useState, useEffect } from "react";
+// import axios from "axios";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        // const response = await axios.get("https://dummyjson.com/products");
+        // setProducts(response.data.products);
+        // setLoading(false);
+
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        setProducts(data.products);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    // return <div>Loading...</div>;
+    return "";
+  }
+
   return (
     <div>
       <Helmet>
@@ -18,7 +48,26 @@ const Products = () => {
         </div>
         <div className="content">
           <div className="container-fluid">
-            <p>Products</p>
+            <div className="row">
+              {products.map((product) => (
+                <div key={product.id} className="col-md-4">
+                  <div className="card mb-4">
+                    <img
+                      src={product.thumbnail}
+                      className="card-img-top"
+                      alt={product.title}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{product.description}</p>
+                      <p className="card-text">
+                        <strong>${product.price}</strong>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
